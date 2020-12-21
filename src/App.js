35b44109beecff6bcf98817bb71c14ee02b6b72a
Glitch (resisted-mercury-corn) <none>
2020-12-21 12:07:21 +0000

    🎭🎨 Checkpoint ./src/App.js:24272135/2658

--- src/App.js
+++ src/App.js
@@ -13,45 +13,42 @@ import {
 
 
 function App() {
-  return (  // AÇO ES JSX
-    <Router>
+  return (
+    <link rel="stylesheet" href="style.css">
 
-      <div className="App">
-        <header className="App-header">
-          <Router>
-            <div>
-              <nav className="nav">
-                <ul>
-                  <li>
-                    <Link to="/">Home</Link>
-                  </li>
-                  <li>
-                    <Link to="/aboutus">AboutsUs</Link>
-                  </li>
-                  <li>
-                    <Link to="/contact">Contact</Link>
-                  </li>
-                </ul>
-              </nav>
-              <Switch>
-                <Route path="/aboutus">
-                  <AboutUs />
-                </Route>
-                <Route path="/contact">
-                  <Contact />
-                </Route>
-                <Route path="/">
-                  <Home />
-                </Route>
-              </Switch>
-            </div>
-          </Router>
-        </header>
-      </div>
+    
 
-    </Router>
+    <h1>kya aap sasta nasha karte ho?</h1>
 
-  );
-}
+    <img src="images/f5a3a43a-e9d0-4500-b922-4c3445a2d54d.png" alt="The nasha: soon surrounding the whole world">
+
+    <p>if yes then pls use following atleast once</p>
+
+    <ul> 
+      <li>dettol</li>
+
+      <li>harpic or lizol</li>
+
+      <li>expired clinic plus</li>
+
+	
+
+    </ul>
+
+    <p>sasta nasha reduces stress,depression,anxiety so pls don't hesitate to try sasta nasha u can order directly with link below</p>
+
+    <u1>
+      <li> buy <a href="https://www.amazon.in/Dettol-Original-Liquid-Handwash-Variant/dp/B06XKRTJ4P/ref=mp_s_a_1_1?dchild=1&keywords=dettol+handwash+bottle&qid=1584942795&s=pantry&sr=8-1">dettol</a> 
+
+      <li> buy <a href="https://www.amazon.in/gp/slredirect/picassoRedirect.html/ref=pa_sp_phone_search_atf_aps_sr_pg1_2?ie=UTF8&adId=A06970821GJO34PERLTKR&url=%2FHarpic-Powerplus-Toilet-Cleaner-Original%2Fdp%2FB00NWFVVG2%2Fref%3Dmp_s_a_1_2_sspa%3Fdchild%3D1%26keywords%3Dharpic%2Bbathroom%2Bcleaner%26qid%3D1584943094%26sprefix%3Dharpic%2B%26sr%3D8-2-spons%26psc%3D1&qualifier=1584943093&id=4182361958139707&widgetName=sp_phone_search_atf">harpic</a> 
+
+      <li> buy <a href="https://www.amazon.in/Clinic-Plus-Strong-Shampoo-Sachet/dp/B01HHSZL24/ref=mp_s_a_1_1?dchild=1&keywords=clinic+plus+sachet&qid=1584943421&sprefix=clinic+plus+sach&sr=8-1">clinic plus</a>
+
+   
+
+  
+	
+)
+  }
 
 export default App;
