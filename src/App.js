9f67f81bf1786a1a78212cbebfb85ccb04e30e31
Glitch (resisted-mercury-corn) <none>
2020-12-21 12:19:15 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,54 +1,9 @@
 import './App.css';
-import ReactDOM from "react-dom";
-import AboutUs from './components/AboutUs'
-import Home from './components/Home'
-import Contact from './components/Contact'
-
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-
 
 function App() {
   return (
-    <link rel="stylesheet" href="style.css">
-
-    
-
-    <h1>kya aap sasta nasha karte ho?</h1>
-
-    <img src="images/f5a3a43a-e9d0-4500-b922-4c3445a2d54d.png" alt="The nasha: soon surrounding the whole world">
-
-    <p>if yes then pls use following atleast once</p>
-
-    <ul> 
-      <li>dettol</li>
-
-      <li>harpic or lizol</li>
-
-      <li>expired clinic plus</li>
-
-	
-
-    </ul>
-
-    <p>sasta nasha reduces stress,depression,anxiety so pls don't hesitate to try sasta nasha u can order directly with link below</p>
-
-    <u1>
-      <li> buy <a href="https://www.amazon.in/Dettol-Original-Liquid-Handwash-Variant/dp/B06XKRTJ4P/ref=mp_s_a_1_1?dchild=1&keywords=dettol+handwash+bottle&qid=1584942795&s=pantry&sr=8-1">dettol</a> 
-
-      <li> buy <a href="https://www.amazon.in/gp/slredirect/picassoRedirect.html/ref=pa_sp_phone_search_atf_aps_sr_pg1_2?ie=UTF8&adId=A06970821GJO34PERLTKR&url=%2FHarpic-Powerplus-Toilet-Cleaner-Original%2Fdp%2FB00NWFVVG2%2Fref%3Dmp_s_a_1_2_sspa%3Fdchild%3D1%26keywords%3Dharpic%2Bbathroom%2Bcleaner%26qid%3D1584943094%26sprefix%3Dharpic%2B%26sr%3D8-2-spons%26psc%3D1&qualifier=1584943093&id=4182361958139707&widgetName=sp_phone_search_atf">harpic</a> 
-
-      <li> buy <a href="https://www.amazon.in/Clinic-Plus-Strong-Shampoo-Sachet/dp/B01HHSZL24/ref=mp_s_a_1_1?dchild=1&keywords=clinic+plus+sachet&qid=1584943421&sprefix=clinic+plus+sach&sr=8-1">clinic plus</a>
-
-   
-
+    <h1> lmao test f</h1>
+    )}
   
 	
-)
-  }
-
 export default App;
